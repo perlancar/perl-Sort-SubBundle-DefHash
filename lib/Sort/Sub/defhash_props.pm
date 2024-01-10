0d9ef0242daea7f40116c6ac9c71be471d0cbd54
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-01-10 15:38:19 +0700

    Initial commit

---
+++ lib/Sort/Sub/defhash_props.pm
@@ -0,0 +1,51 @@
+package Sort::Sub::rinci_sub_meta_props;
+
+use 5.010001;
+use strict;
+use warnings;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+sub meta {
+    return {
+        v => 1,
+        summary => 'Sort DefHash properties and attributes',
+        description => <<'MARKDOWN',
+
+Known (standard) properties and attributes are put first, in order of mention in the spec.
+Then unknown properties/attributes are sorted asciibetically.
+
+MARKDOWN
+    };
+}
+
+sub gen_sorter {
+    require Sort::BySpec;
+
+    my ($is_reverse, $is_ci) = @_;
+
+    Sort::BySpec::cmp_by_spec(
+        spec => [
+            'v',
+            'defhash_v',
+            'name', qr/\Aname\./,
+            'caption', qr/\Acaption\./,
+            'description', qr/\Adescription\./,
+            'tags',
+            'default_lang',
+            'x', qr/\Ax\./,
+            sub { $_[0] cmp $_[1] },
+        ],
+        reverse => $is_reverse,
+    );
+}
+
+1;
+# ABSTRACT:
+
+=for Pod::Coverage ^(gen_sorter|meta)$
+
+=cut
---
+++ lib/Sort/SubBundle/DefHash.pm
@@ -0,0 +1,14 @@
+## no critic: TestingAndDebugging::RequireUseStrict
+package Sort::SubBundle::DefHash;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Sort::Sub subroutines related to DefHash
+
+=head1 SEE ALSO
+
+L<DefHash>
